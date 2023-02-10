f5a1c840ab0c13858ea1acf93c2d25cb6290a274
Richard van Dusschoten <richardd@mediamonks.com>
2023-02-10 13:57:03 +0100

    Prefabbed lamppost, changes to Post FX

--- Assets/Lamppost.cs
+++ Assets/Lamppost.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,24 +6,34 @@ using UnityEngine;
 public class Lamppost : MonoBehaviour
 {
 
-    [SerializeField] private GameObject lamp1;
-    [SerializeField] private GameObject lamp2;
-    [SerializeField] private Light pointLight;
-
+    [SerializeField] private GameObject _lamp1;
+    [SerializeField] private GameObject _lamp2;
+    [SerializeField] private Light _spotLight;
+    
     private Material _lamp1Mat;
     private Material _lamp2Mat;
+    private SphereCollider _collider;
+
+    private float _lightTimer;
+    private float _maxLightTime = 20f;
 
     // Start is called before the first frame update
     void Start()
     {
-        _lamp1Mat = lamp1.GetComponent<Renderer>().material;
-        _lamp2Mat = lamp2.GetComponent<Renderer>().material;
-        
+        _lamp1Mat = _lamp1.GetComponent<Renderer>().material;
+        _lamp2Mat = _lamp2.GetComponent<Renderer>().material;
+        _collider = GetComponent<SphereCollider>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
+    {
+        _lightTimer = Mathf.Clamp(_lightTimer - Time.deltaTime, 0f, _maxLightTime);
+    }
+
+    private void ToggleLight(bool on)
     {
         
     }
+    
+    
 }
