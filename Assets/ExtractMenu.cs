5492651c0df55c8749030901930a0ecd0aa4c45c
Richard van Dusschoten <richardd@mediamonks.com>
2023-02-19 13:20:45 +0100

    Added buttons to ExtractMenu

--- Assets/ExtractMenu.cs
+++ Assets/ExtractMenu.cs
@@ -1,9 +1,13 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class ExtractMenu : MonoBehaviour
 {
+    [SerializeField] private TextMeshProUGUI _extractText;
+
     public void NextLevel()
     {
         GameOverseer.Instance.SetGameState(GameState.StartGame);
