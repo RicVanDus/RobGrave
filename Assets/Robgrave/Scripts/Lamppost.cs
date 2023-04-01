8cc823badd8f09ebc7ce3ee22843523fa1d111aa
Richard van Dusschoten <richardd@mediamonks.com>
2023-04-01 20:06:11 +0200

    Lamppost GUI tween animation for Gui visiblity

--- Assets/Robgrave/Scripts/Lamppost.cs
+++ Assets/Robgrave/Scripts/Lamppost.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using DG.Tweening;
 using DG.Tweening.Core.Easing;
 using UnityEngine;
 using UnityEngine.UIElements;
@@ -19,6 +20,7 @@ public class Lamppost : MonoBehaviour
     private Material _lamp2Mat;
     private GUI_lamppost _GUI_lamppost;
     private bool _guiVisible = false;
+    private Vector3 _guiDefaultScale;
 
     private float _lightTimer;
     private float _maxLightTime = 2f;
@@ -29,6 +31,7 @@ public class Lamppost : MonoBehaviour
     private bool _playerCanInteract = false;
     private bool _playerIsInteracting = false;
     private bool _lightIsflashing = false;
+    
 
     // Start is called before the first frame update
     void Start()
@@ -36,6 +39,7 @@ public class Lamppost : MonoBehaviour
         _lamp1Mat = _lamp1.GetComponent<Renderer>().material;
         _lamp2Mat = _lamp2.GetComponent<Renderer>().material;
         _GUI_lamppost = _UI.GetComponent<GUI_lamppost>();
+        _guiDefaultScale = _UI.transform.localScale;
     }
 
     private void Update()
@@ -62,8 +66,7 @@ public class Lamppost : MonoBehaviour
 
             if (!_guiVisible)
             {
-                _GUI_lamppost.ShowGraphic = true;
-                _guiVisible = true;                
+                SetGUIVisible(true);           
             }
 
         }
@@ -94,8 +97,7 @@ public class Lamppost : MonoBehaviour
         {
             if (_guiVisible)
             {
-                _GUI_lamppost.ShowGraphic = false;
-                _guiVisible = false;
+                SetGUIVisible(false);
             }
         }
         else
@@ -192,6 +194,27 @@ public class Lamppost : MonoBehaviour
         if (other.CompareTag("Player"))
         {
             _playerCanInteract = false;
+        }
+    }
+
+    private void SetGUIVisible(bool visible)
+    {
+        if (visible)
+        {
+            _GUI_lamppost.ShowGraphic = true;
+            _UI.transform.localScale = Vector3.zero;
+
+            _UI.transform.DOScale(_guiDefaultScale, 0.3f).SetEase(Ease.InExpo);
+            _guiVisible = true;
+        }
+        else
+        {
+            _UI.transform.DOScale(Vector3.zero, 0.3f).SetEase(Ease.InExpo).OnComplete(()=>
+            {
+                _GUI_lamppost.ShowGraphic = false;
+            });
+            
+            _guiVisible = false;
         }
     }
 }
