703bd03ea9ddca9b9c26f338c249bd9cc310060e
Richard van Dusschoten <richardd@mediamonks.com>
2023-09-27 09:41:58 +0200

    3DUI: Score mesh and script updated

--- Assets/UIScore.cs
+++ Assets/UIScore.cs
@@ -1,5 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
+using DG.Tweening;
+using TMPro;
 using UnityEngine;
 
 public class UIScore : MonoBehaviour
@@ -9,27 +11,30 @@ public class UIScore : MonoBehaviour
     [SerializeField] private GameObject _nrRotator3;
     [SerializeField] private GameObject _nrRotator4;
     [SerializeField] private GameObject _nrRotator5;
-    
-    
-    
+
+    [SerializeField] private GameObject _preScoreObj;
+    [SerializeField] private TextMeshProUGUI _preScoreText;
+    private bool _preScoreOut;
+
+    [SerializeField] private GameObject _reqScoreObj;
+    [SerializeField] private TextMeshProUGUI _reqScoreText;
+    [SerializeField] private Light _reqScoreLight;
+    private bool _reqScoreMet;
+    private int _reqScore;
+
+    private Color _redColor = Color.red;
+    private Color _greenColor = Color.green;
+
     void Start()
     {
-        
+        _reqScore = GameManager.Instance.thisLevel.valuablesRequired;
     }
 
-    void Update()
-    {
-        
-    }
-
-    
     public void UpdateScore(int scoreAdded)
     {
-        if (scoreAdded > 0)
-        {
-            
-        }
-        else if (scoreAdded < 0)
+        int score = PlayerController.Instance.score;
+        
+        if (scoreAdded != 0)
         {
             
         }
@@ -37,5 +42,29 @@ public class UIScore : MonoBehaviour
         {
             return;
         }
+
+        if (score >= _reqScore && !_reqScoreMet)
+        {
+            ReqScoreToggle(true);
+            _reqScoreMet = true;
+        }
+        else if (score < _reqScore && _reqScoreMet)
+        {
+            ReqScoreToggle(false);
+            _reqScoreMet = false;
+        }
+    }
+
+    private void ReqScoreToggle(bool isMet)
+    {
+        // toggle the light and textcolor
+    }
+
+    private void RotateNumber(GameObject NrRotator, int amount)
+    {
+        Vector3 rot = NrRotator.transform.eulerAngles;
+        rot.y += 36f * amount;
+
+        NrRotator.transform.DORotate(rot, 3f).SetEase(Ease.OutExpo);
     }
 }
