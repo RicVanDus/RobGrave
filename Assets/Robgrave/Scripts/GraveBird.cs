406f45beb12934f7ef7a51ae487408907ab64fa4
Richard van Dusschoten <richardd@mediamonks.com>
2023-12-09 13:02:45 +0100

    Changes to gravebird

--- Assets/Robgrave/Scripts/GraveBird.cs
+++ Assets/Robgrave/Scripts/GraveBird.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using DG.Tweening;
 using Unity.VisualScripting;
 using UnityEngine;
 
@@ -10,7 +11,6 @@ public class GraveBird : MonoBehaviour
     // when crypt key grave is dug = flies out of frame
     // when grave 2 is looted then flies directly to grave 3
     
-    private Rigidbody _rigidbody;
     [SerializeField] private float _speed = 0.1f;
     [SerializeField] private float _smoothing = 1f;
     private int _currentTarget = 0; // out of 3
@@ -24,10 +24,11 @@ public class GraveBird : MonoBehaviour
     private Vector3 _velocity;
     
     private WaitForSeconds _softTick = new (0.5f);
+    private Animator _anim;
 
     private void Awake()
     {
-        _rigidbody = GetComponent<Rigidbody>();
+        _anim = GetComponent<Animator>();
     }
 
     private void Update()
@@ -46,29 +47,41 @@ public class GraveBird : MonoBehaviour
         StartCoroutine(CheckGraveStatus());
     }
     
+    // that's cute. BUT ITS WRONG
     private void GoToTarget(Transform target)
     {
         _target = target.position;
+        _anim.SetBool("TakingOff", true);
         _birdIsFlying = true;
+        Vector3 rotateTowards = Vector3.RotateTowards(transform.eulerAngles, _target, 10f, 10f);
+        rotateTowards.x = transform.eulerAngles.x;
+        rotateTowards.z = transform.eulerAngles.z;
+        
+        transform.DORotate(rotateTowards, 0.7f);
     }
 
     private void Flying()
     {
+        Vector3 posDif;
+        Vector3 _lastPosition;
+        _lastPosition = transform.position;
+        
         transform.position =
             Vector3.SmoothDamp(transform.position, _target, ref _velocity, _smoothing, _speed, Time.deltaTime);
 
         if (Vector3.Distance(transform.position, _target) < 0.1f)
         {
             _birdIsFlying = false;
+            _anim.SetBool("TakingOff", false);
         }
-        
-        //rotate towards goal, flying animation started with speed influenced by _velocity.magnitude
 
+        //calculate velocity?
+        posDif = _lastPosition - transform.position;
+        var velocity = posDif.magnitude;
+        
+        _anim.SetFloat("Velocity", velocity);
+        Debug.Log("SPEED bird: " + velocity);
     } 
-    
-    
-    
-    
     
     // coroutine checks for changes in current target & changes in last target. 
     // if changed check for next target state and then fly there
