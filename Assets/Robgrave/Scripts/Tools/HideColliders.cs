f45a731cc5a70a410f5efccf2a30855d8d7952f4
Richard van Dusschoten <richardd@mediamonks.com>
2025-01-08 12:57:14 +0100

    Small fixes to bushes and tool scripts working again

---
+++ Assets/Robgrave/Scripts/Tools/HideColliders.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+#if UNITY_EDITOR
+
+[ExecuteAlways]
+
+public class HideColliders : MonoBehaviour
+{
+    private bool _visible = false;
+    
+    public void ToggleColliders()
+    {
+        if (_visible)
+        {
+            Debug.Log("[colliders not visible]");
+            _visible = false;
+            ToggleAllMeshRenderers(false);
+        }
+        else
+        {
+            _visible = true;
+            Debug.Log("[colliders visible]");
+            ToggleAllMeshRenderers(true);
+        }
+    }
+
+    private void ToggleAllMeshRenderers(bool toggle)
+    {
+        var children =  transform.GetComponentsInChildren<Renderer>();
+        
+        foreach (Renderer child in children)
+        {
+            child.enabled = toggle;
+        }
+    }
+}
+
+[ExecuteAlways]
+
+[CustomEditor(typeof(HideColliders))]
+public class ColliderCreatorEditor : Editor {
+   
+    override public void  OnInspectorGUI ()
+    {
+        HideColliders hideColliders = (HideColliders)target;
+        if(GUILayout.Button("Toggle Colliders"))
+        {
+            hideColliders.ToggleColliders();
+        }
+        DrawDefaultInspector();
+    }
+}
+#endif
